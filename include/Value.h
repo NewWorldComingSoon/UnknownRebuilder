636382537c86d443ecf3a4c80c468b0c4c0d4caf
gmh5225 <2315157@qq.com>
2022-12-12 03:25:56 +0800

    Update

--- include/Value.h
+++ include/Value.h
@@ -1,13 +1,32 @@
 #pragma once
 #include "Object.h"
+#include "Type.h"
+
+#include <stdint.h>
+#include <unordered_set>
 
 namespace uir {
 
 class Value : public Object
 {
+private:
+    Type *mType;
+    std::string mValueName;
+    std::unordered_set<Value *> mUsers;
+
 public:
     Value();
+    Value(Type *Ty, const std::string ValueName);
     virtual ~Value();
+
+public:
+    // Get/Set the name of the value
+    std::string getName() const;
+    void setName(const std::string ValueName);
+
+    // Get/Set the type of the value
+    Type *getType() const;
+    void setType(Type *Ty);
 
 private:
 };
--- src/UnknownIR.Value.cpp
+++ src/UnknownIR.Value.cpp
@@ -1,8 +1,46 @@
 #include <Value.h>
 
 namespace uir {
+////////////////////////////////////////////////////////////
+// Ctor/Dtor
+Value::Value() : mType(nullptr), mValueName("")
+{
+    mUsers.clear();
+}
 
-Value::Value() {}
+Value::Value(Type *Ty, const std::string ValueName) : mType(Ty), mValueName(ValueName)
+{
+    mUsers.clear();
+}
+
 Value::~Value() {}
+
+////////////////////////////////////////////////////////////
+// Get/Set
+// Get/Set the name of the value
+std::string
+Value::getName() const
+{
+    return mValueName;
+}
+
+void
+Value::setName(const std::string ValueName)
+{
+    mValueName = ValueName;
+}
+
+// Get/Set the type of the value
+Type *
+Value::getType() const
+{
+    return mType;
+}
+
+void
+Value::setType(Type *Ty)
+{
+    mType = Ty;
+}
 
 } // namespace uir
