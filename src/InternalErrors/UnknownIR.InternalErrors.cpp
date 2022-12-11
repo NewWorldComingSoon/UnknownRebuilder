bdf5dc7e74ed842b515bc885837ab9edc6dec319
gmh5225 <2315157@qq.com>
2022-12-11 19:26:04 +0800

    Refact 'uir_unreachable_internal'

--- src/InternalErrors/UnknownIR.InternalErrors.cpp
+++ src/InternalErrors/UnknownIR.InternalErrors.cpp
@@ -6,19 +6,19 @@ namespace uir {
 void
 uir_unreachable_internal(const char *msg /*= nullptr*/, const char *file /*= nullptr*/, unsigned line /*= 0*/)
 {
-    std::cerr << "[UIR]: ";
     if (msg)
     {
+        std::cerr << "[UIR]: ";
         std::cerr << msg << "\n";
+        std::cerr << "UNREACHABLE executed";
+        if (file)
+        {
+            std::cerr << " at " << file << ":" << line;
+        }
+
+        std::cerr << "!\n";
     }
 
-    std::cerr << "UNREACHABLE executed";
-    if (file)
-    {
-        std::cerr << " at " << file << ":" << line;
-    }
-
-    std::cerr << "!\n";
     std::abort();
 }
 
