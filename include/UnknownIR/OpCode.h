a342f9575720ae215fcf2b0aaa2a7ffb01ff66db
gmh5225 <2315157@qq.com>
2022-12-18 00:03:42 +0800

    Update

--- include/UnknownIR/OpCode.h
+++ include/UnknownIR/OpCode.h
@@ -1,11 +1,11 @@
 #pragma once
-#include <string>
 #include <cstdint>
+#include <string>
+#include <array>
 
 namespace uir {
-#define GET_OPCODE_NAME(Name) (#Name)
 
-enum OpCodeID : uint8_t
+enum class OpCodeID : uint8_t
 {
     // Binary Operators
     Add,
@@ -21,24 +21,23 @@ enum OpCodeID : uint8_t
 
 struct OpCodeComponent
 {
-    std::string mOpCodeName;
     OpCodeID mOpCodeID;
+    std::string mOpCodeName;
     uint32_t mNumberOfOperands;
     bool mHasEFlags;
 };
 
-__declspec(selectany) OpCodeComponent gOpCodeComponent[] = {
+const OpCodeComponent GlobalOpCodeComponents[] = {
     // Binary Operators
-    {GET_OPCODE_NAME(Add), Add, 3, true},
-    {GET_OPCODE_NAME(Sub), Sub, 3, true},
+    {OpCodeID::Add, "Add", 3, true},
+    {OpCodeID::Sub, "Sub", 3, true},
 
     // Terminator
-    {GET_OPCODE_NAME(Ret), Ret, 0, false},
-    {GET_OPCODE_NAME(RetIMM), RetIMM, 1, false},
+    {OpCodeID::Ret, "Ret", 0, false},
+    {OpCodeID::RetIMM, "RetIMM", 1, false},
 
     // Unknown
-    {GET_OPCODE_NAME(Unknown), Unknown, 0, false},
+    {OpCodeID::Unknown, "Unknown", 0, false},
 };
 
-#undef GET_OPCODE_NAME
 } // namespace uir
---
+++ test/test-uir/test.inst.cpp
@@ -0,0 +1,19 @@
+#include <UnknownIR.h>
+#include <gtest/gtest.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace uir;
+
+TEST(test_uir, test_uir_inst_1)
+{
+    Context CTX;
+    CTX.setArch(Context::ArchX86);
+    CTX.setMode(Context::Mode64);
+
+    size_t Size = sizeof(GlobalOpCodeComponents) / sizeof(GlobalOpCodeComponents[0]);
+    for (size_t i = 0; i < Size; ++i)
+    {
+        printf("OpCodeName = %s\n", GlobalOpCodeComponents[i].mOpCodeName.c_str());
+    }
+}
