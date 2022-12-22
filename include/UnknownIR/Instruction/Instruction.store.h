c30fd0d5a0ee025560a7d160d83c08fe3c37581e
gmh5225 <2315157@qq.com>
2022-12-22 14:19:00 +0800

    Update

--- include/UnknownIR/Instruction/Instruction.store.h
+++ include/UnknownIR/Instruction/Instruction.store.h
@@ -17,13 +17,13 @@ public:
 public:
     // Virtual
     // Get the opcode name of this instruction
-    virtual unknown::StringRef getOpcodeName() const;
+    virtual unknown::StringRef getOpcodeName() const override;
 
     // Get the default number of operands
-    virtual uint32_t getDefaultNumberOfOperands() const;
+    virtual uint32_t getDefaultNumberOfOperands() const override;
 
     // Is this instruction with flags?
-    virtual bool hasFlags() const;
+    virtual bool hasFlags() const override;
 
 public:
     // Get/Set
