b57dbbeebc0ee1cf3351734d6c5e30e6945d96a2
gmh5225 <2315157@qq.com>
2022-12-24 01:29:11 +0800

    Update

--- test/test-uir/test.utils.cpp
+++ test/test-uir/test.utils.cpp
@@ -11,6 +11,6 @@ TEST(test_uir, test_uir_utils_1)
 {
     std::error_code ec;
     unknown::raw_fd_ostream out(UNKNOWN_REBUILDER_SRC_DIR R"(/test_uir_utils_1.TXT)", ec, unknown::sys::fs::F_None);
-    out << unknown::hexdigit(11);
+    out << "0x" << unknown::hexdigit(11);
     out.close();
 }
