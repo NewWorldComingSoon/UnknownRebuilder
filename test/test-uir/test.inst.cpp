ada3e892935e0402f6579e9e431c8aa0bb47d9ef
gmh5225 <2315157@qq.com>
2022-12-23 13:53:32 +0800

    Update

--- test/test-uir/test.inst.cpp
+++ test/test-uir/test.inst.cpp
@@ -1,7 +1,7 @@
 #include <UnknownIR.h>
 #include <gtest/gtest.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <format>
+#include <iostream>
 
 using namespace uir;
 
@@ -11,17 +11,17 @@ TEST(test_uir, test_uir_inst_1)
     CTX.setArch(Context::ArchX86);
     CTX.setMode(Context::Mode64);
 
-    printf("RetIMMOpCodeName = %s\n", RetIMMComponent.mOpCodeName.data());
+    std::cout << "RetIMMOpCodeName = " << std::format("{}", RetIMMComponent.mOpCodeName.data()) << std::endl;
 
     auto ReturnImmIst = ReturnImmInst::get(ConstantInt::get(CTX, 1, 32));
     auto ImmCstInt = ReturnImmIst->getImmConstantInt();
     if (ImmCstInt)
     {
-        printf("ImmCstInt->getZExtValue() = %lld\n", ImmCstInt->getZExtValue());
+        std::cout << "ImmCstInt->getZExtValue() = " << std::format("{}", ImmCstInt->getZExtValue()) << std::endl;
 
         // Update op1
         ReturnImmIst->setImmConstantInt(ConstantInt::get(CTX, 2, 32));
         auto ImmCstInt2 = ReturnImmIst->getImmConstantInt();
-        printf("ImmCstInt2->getZExtValue() = %lld\n", ImmCstInt2->getZExtValue());
+        std::cout << "ImmCstInt2->getZExtValue() = " << std::format("{}", ImmCstInt->getZExtValue()) << std::endl;
     }
 }
