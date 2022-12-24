2d81593073104177dee9c452b8e3cf0e686dba02
gmh5225 <2315157@qq.com>
2022-12-24 23:17:37 +0800

    Update

--- include/UnknownIR/Instruction/Instruction.terminator.h
+++ include/UnknownIR/Instruction/Instruction.terminator.h
@@ -6,11 +6,37 @@ namespace uir {
 
 class TerminatorInst : public Instruction
 {
+public:
+    using SuccessorsListType = std::vector<BasicBlock *>;
+
+protected:
+    SuccessorsListType mSuccessorsList;
+
 protected:
     TerminatorInst(OpCodeID OpCodeId);
     virtual ~TerminatorInst();
 
 public:
+    // SuccessorsList
+    // Returns the list of successors of this terminator instruction
+    SuccessorsListType &getSuccessorsList() { return mSuccessorsList; }
+    const SuccessorsListType &getSuccessorsList() const { return mSuccessorsList; }
+
+public:
+    // Iterator
+    using successor_iterator = SuccessorsListType::iterator;
+    using const_successor_iterator = SuccessorsListType::const_iterator;
+    successor_iterator successor_begin();
+    const_successor_iterator successor_begin() const;
+    successor_iterator successor_end();
+    const_successor_iterator successor_end() const;
+    BasicBlock *successor_back();
+    BasicBlock *successor_front();
+    void successor_push(BasicBlock *BB);
+    void successor_pop();
+    size_t successor_count() const;
+    void successor_erase(BasicBlock *BB);
+    bool successor_empty() const;
 };
 
 } // namespace uir
--- src/UnknownIR/Instruction/Instruction.terminator.cpp
+++ src/UnknownIR/Instruction/Instruction.terminator.cpp
@@ -11,4 +11,79 @@ TerminatorInst::~TerminatorInst()
     //
 }
 
+////////////////////////////////////////////////////////////
+// Iterator
+TerminatorInst::successor_iterator
+TerminatorInst::successor_begin()
+{
+    return mSuccessorsList.begin();
+}
+
+TerminatorInst::const_successor_iterator
+TerminatorInst::successor_begin() const
+{
+    return mSuccessorsList.cbegin();
+}
+
+TerminatorInst::successor_iterator
+TerminatorInst::successor_end()
+{
+    return mSuccessorsList.end();
+}
+
+TerminatorInst::const_successor_iterator
+TerminatorInst::successor_end() const
+{
+    return mSuccessorsList.cend();
+}
+
+BasicBlock *
+TerminatorInst::successor_back()
+{
+    return mSuccessorsList.back();
+}
+
+BasicBlock *
+TerminatorInst::successor_front()
+{
+    return mSuccessorsList.front();
+}
+
+void
+TerminatorInst::successor_push(BasicBlock *BB)
+{
+    return mSuccessorsList.push_back(BB);
+}
+
+void
+TerminatorInst::successor_pop()
+{
+    return mSuccessorsList.pop_back();
+}
+
+size_t
+TerminatorInst::successor_count() const
+{
+    return mSuccessorsList.size();
+}
+
+void
+TerminatorInst::successor_erase(BasicBlock *BB)
+{
+    for (auto It = mSuccessorsList.begin(); It != mSuccessorsList.end(); ++It)
+    {
+        if (*It == BB)
+        {
+            mSuccessorsList.erase(It);
+            break;
+        }
+    }
+}
+
+bool
+TerminatorInst::successor_empty() const
+{
+    return mSuccessorsList.empty();
+}
+
 } // namespace uir
