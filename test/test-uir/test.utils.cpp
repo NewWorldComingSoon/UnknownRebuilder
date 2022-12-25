bce9fdeab735fcf9d51db01424d522617ce6e1b8
gmh5225 <2315157@qq.com>
2022-12-25 22:42:55 +0800

    Update

--- test/test-uir/test.utils.cpp
+++ test/test-uir/test.utils.cpp
@@ -12,9 +12,10 @@
 TEST(test_uir, test_uir_utils_1)
 {
     std::error_code EC;
-    unknown::raw_fd_ostream Out(UNKNOWN_REBUILDER_SRC_DIR R"(/test_uir_utils_1.TXT)", EC, unknown::sys::fs::F_None);
-    Out << "0x" << unknown::APInt(32, 0x401B00).toString(16, false);
-    Out << "\n";
-    Out << unknown::APInt(32, 0x401B0C).toString(10, false);
-    Out.close();
+    // unknown::raw_fd_ostream Out(UNKNOWN_REBUILDER_SRC_DIR R"(/test_uir_utils_1.TXT)", EC, unknown::sys::fs::F_None);
+    unknown::outs() << "0x" << unknown::APInt(32, 0x401B00).toString(16, false);
+    unknown::outs() << "\n";
+    unknown::outs() << unknown::APInt(32, 0x401B0C).toString(10, false);
+    unknown::outs() << "\n";
+    // Out.close();
 }
