98f06f4ea1f0cb6bc9137f122347af06c3248f3b
gmh5225 <2315157@qq.com>
2022-12-25 22:39:33 +0800

    Update

--- test/test-uir/test.utils.cpp
+++ test/test-uir/test.utils.cpp
@@ -4,6 +4,8 @@
 #include <format>
 #include <iostream>
 
+#include <UnknownUtils/unknown/ADT/APInt.h>
+
 #include <UnknownUtils/unknown/Support/raw_ostream.h>
 #include <UnknownUtils/unknown/Support/FileSystem.h>
 
@@ -11,8 +13,8 @@ TEST(test_uir, test_uir_utils_1)
 {
     std::error_code EC;
     unknown::raw_fd_ostream Out(UNKNOWN_REBUILDER_SRC_DIR R"(/test_uir_utils_1.TXT)", EC, unknown::sys::fs::F_None);
-    Out << "0x" << unknown::utohexstr(0x401000);
+    Out << "0x" << unknown::APInt(32, 0x401B00).toString(16, false);
     Out << "\n";
-    Out << unknown::utostr(0x401000);
+    Out << unknown::APInt(32, 0x401B0C).toString(10, false);
     Out.close();
 }
