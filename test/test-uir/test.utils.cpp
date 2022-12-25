acb6254cff30b05e93d35833cff180ecd8979080
gmh5225 <2315157@qq.com>
2022-12-25 22:43:12 +0800

    Update

--- test/test-uir/test.utils.cpp
+++ test/test-uir/test.utils.cpp
@@ -11,8 +11,8 @@
 
 TEST(test_uir, test_uir_utils_1)
 {
-    std::error_code EC;
-    // unknown::raw_fd_ostream Out(UNKNOWN_REBUILDER_SRC_DIR R"(/test_uir_utils_1.TXT)", EC, unknown::sys::fs::F_None);
+    // std::error_code EC;
+    //  unknown::raw_fd_ostream Out(UNKNOWN_REBUILDER_SRC_DIR R"(/test_uir_utils_1.TXT)", EC, unknown::sys::fs::F_None);
     unknown::outs() << "0x" << unknown::APInt(32, 0x401B00).toString(16, false);
     unknown::outs() << "\n";
     unknown::outs() << unknown::APInt(32, 0x401B0C).toString(10, false);
