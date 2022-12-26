12127095b8ae2cd1ba213974d8bf54c4a4545aa7
gmh5225 <2315157@qq.com>
2022-12-27 01:13:34 +0800

    Update

--- test/test-uir/test.bb.cpp
+++ test/test-uir/test.bb.cpp
@@ -11,6 +11,16 @@ TEST(test_uir, test_uir_bb_1)
         Context CTX;
         CTX.setArch(Context::ArchX86);
         CTX.setMode(Context::Mode64);
+
+        auto BB1 = BasicBlock::create(CTX, "bb1", 0x401000, 0x401005);
+        auto RetInst = ReturnInstruction::get();
+        RetInst->setInstructionAddress(0x401000);
+        BB1->insertInst(RetInst);
+        BB1->insertInst(RetInst);
+        for (auto I : *BB1)
+        {
+            unknown::outs() << *I;
+        }
     }
 
     std::cout << "--------------------bp-----------------------\n";
