be51df63a988d74d236fed22410f11b7c5d3a49d
gmh5225 <2315157@qq.com>
2022-12-27 05:02:53 +0800

    Update

--- include/UnknownIR/GlobalVariable.h
+++ include/UnknownIR/GlobalVariable.h
@@ -36,4 +36,32 @@ public:
     static GlobalVariable *get(Type *Ty);
 };
 
+template <typename T>
+class GlobalArray : public GlobalVariable
+{
+public:
+    using GlobalArrayType = std::vector<T>;
+
+private:
+    GlobalArrayType mElements;
+
+public:
+    GlobalArray(
+        Context &C,
+        Type *ElmtTy,
+        unknown::StringRef GlobalArrayName,
+        const GlobalArrayType &GlobalArrayElements,
+        uint64_t GlobalArrayAddress = 0) :
+        GlobalVariable(PointerType::get(C, ElmtTy), GlobalArrayName, GlobalArrayAddress), mElements(GlobalArrayElements)
+    {
+    }
+    virtual ~GlobalArray() {}
+
+public:
+    // Get/Set
+    GlobalArrayType &getGlobalArray() { return mElements; }
+    const GlobalArrayType &getGlobalArray() const { return mElements; }
+    void setGlobalArray(const GlobalArrayType &GlobalArrayElements) { mElements = GlobalArrayElements; }
+};
+
 } // namespace uir
