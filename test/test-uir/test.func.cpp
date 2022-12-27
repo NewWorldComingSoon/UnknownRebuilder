4c0b73fdf3f578ec25ca3d959b720f0a5cf6ef44
gmh5225 <2315157@qq.com>
2022-12-28 00:30:04 +0800

    Update

--- test/test-uir/test.func.cpp
+++ test/test-uir/test.func.cpp
@@ -11,6 +11,8 @@ TEST(test_uir, test_uir_func_1)
         Context CTX;
         CTX.setArch(Context::ArchX86);
         CTX.setMode(Context::Mode64);
+
+        // TODO
     }
 
     std::cout << "--------------------bp-----------------------" << std::endl;
