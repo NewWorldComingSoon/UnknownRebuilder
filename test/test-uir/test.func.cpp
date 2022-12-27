4b9a70ab98cd79245dbc7ccd8af5d3061a08fbef
gmh5225 <2315157@qq.com>
2022-12-28 00:22:33 +0800

    Update

---
+++ test/test-uir/test.func.cpp
@@ -0,0 +1,17 @@
+#include <UnknownIR.h>
+#include <gtest/gtest.h>
+#include <format>
+#include <iostream>
+
+using namespace uir;
+
+TEST(test_uir, test_uir_func_1)
+{
+    {
+        Context CTX;
+        CTX.setArch(Context::ArchX86);
+        CTX.setMode(Context::Mode64);
+    }
+
+    std::cout << "--------------------bp-----------------------\n";
+}
