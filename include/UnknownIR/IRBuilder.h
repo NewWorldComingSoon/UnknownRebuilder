a90d4cf62bc3feb08a5174f4ec836c7536993d5b
gmh5225 <2315157@qq.com>
2023-01-05 16:20:42 +0800

    Update

--- include/UnknownIR/IRBuilder.h
+++ include/UnknownIR/IRBuilder.h
@@ -1,6 +1,39 @@
 #pragma once
 #include <UnknownIR/Instruction.h>
+#include <UnknownIR/BasicBlock.h>
 
 namespace uir {
 
-}
+class IRBuilderDefaultInserter
+{
+protected:
+    void InsertHelper(Instruction *I, uint64_t InstAddress, BasicBlock *BB, BasicBlock::iterator InsertPt) const;
+};
+
+class IRBuilderBase
+{
+protected:
+    BasicBlock *mBB;
+    BasicBlock::iterator mInsertPt;
+    Context &mContext;
+
+public:
+    IRBuilderBase(Context &C);
+
+public:
+    // Get/Set
+    Context &getContext() const { return mContext; }
+    BasicBlock *getInsertBlock() const { return mBB; }
+    BasicBlock::iterator getInsertPoint() const { return mInsertPt; }
+
+public:
+    // Insertion Point
+    // Clear the insertion point
+    void clearInsertionPoint();
+
+    // Set the insertion point
+    void setInsertPoint(BasicBlock *BB);
+    void setInsertPoint(Instruction *I);
+};
+
+} // namespace uir
--- src/UnknownIR/UnknownIR.IRBuilder.cpp
+++ src/UnknownIR/UnknownIR.IRBuilder.cpp
@@ -1,5 +1,77 @@
 #include <IRBuilder.h>
 
 namespace uir {
+////////////////////////////////////////////////////////////
+//     IRBuilderDefaultInserter
+//
+void
+IRBuilderDefaultInserter::InsertHelper(
+    Instruction *I,
+    uint64_t InstAddress,
+    BasicBlock *BB,
+    BasicBlock::iterator InsertPt) const
+{
+    if (I)
+    {
+        I->setInstructionAddress(InstAddress);
+    }
 
+    if (BB)
+    {
+        BB->getInstList().insert(InsertPt, I);
+        if (I)
+        {
+            I->setParent(BB);
+        }
+    }
 }
+
+////////////////////////////////////////////////////////////
+//     IRBuilderBase
+//
+IRBuilderBase::IRBuilderBase(Context &C) : mContext(C)
+{
+    clearInsertionPoint();
+}
+
+////////////////////////////////////////////////////////////
+// Insertion Point
+// Clear the insertion point
+void
+IRBuilderBase::clearInsertionPoint()
+{
+    mBB = nullptr;
+    (*mInsertPt) = nullptr;
+}
+
+// Set the insertion point
+void
+IRBuilderBase::setInsertPoint(BasicBlock *BB)
+{
+    assert(BB != nullptr && "BB != nullptr");
+
+    mBB = BB;
+    mInsertPt = BB->end();
+}
+
+void
+IRBuilderBase::setInsertPoint(Instruction *I)
+{
+    assert(I != nullptr && "I != nullptr");
+
+    auto BB = I->getParent();
+    assert(BB != nullptr && "BB != nullptr");
+
+    mBB = BB;
+
+    for (auto It = BB->begin(); It != BB->end(); ++It)
+    {
+        if ((*It) == I)
+        {
+            mInsertPt = It;
+            return;
+        }
+    }
+}
+
+} // namespace uir
