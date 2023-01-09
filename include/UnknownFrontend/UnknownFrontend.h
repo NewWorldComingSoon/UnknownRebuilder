c45260d9e642b8508c36a2c31265d4f5e3fddf8e
gmh5225 <2315157@qq.com>
2023-01-09 22:44:10 +0800

    Update

--- include/UnknownFrontend/UnknownFrontend.h
+++ include/UnknownFrontend/UnknownFrontend.h
@@ -16,6 +16,43 @@ public:
     virtual ~UnknownFrontendTranslator() = default;
 
 public:
+    // Translate
+    // Translate the given binary into UnknownIR
+    virtual std::unique_ptr<uir::Module> translateBinary(const std::string &ModuleName) = 0;
+
+    // Translate one instruction into UnknownIR
+    virtual bool translateOneInstruction(const uint8_t *Bytes, size_t Size, uint64_t Address, uir::BasicBlock *BB) = 0;
+
+    // Translate one BasicBlock into UnknownIR
+    virtual uir::BasicBlock *translateOneBasicBlock(const std::string &BlockName, uint64_t Address) = 0;
+
+    // Translate one Function into UnknownIR
+    virtual uir::Function *translateOneFunction(const std::string &FunctionName, uint64_t Address) = 0;
+
+public:
+    // Get/Set
+    // Get the context of this translator
+    virtual uir::Context &getContext() const = 0;
+
+    // Get the Binary File
+    virtual const std::string &getBinaryFile() const = 0;
+
+    // Get the Symbol File
+    virtual const std::string &getSymbolFile() const = 0;
+
+    // Get the begin of current pointer
+    virtual const uint64_t getCurPtrBegin() const = 0;
+
+    // Get the end of current pointer
+    virtual const uint64_t getCurPtrEnd() const = 0;
+
+    // Set the begin of current pointer
+    virtual void setCurPtrBegin(uint64_t Ptr) = 0;
+
+    // Set the end of current pointer
+    virtual void setCurPtrEnd(uint64_t Ptr) = 0;
+
+public:
     // Static
     static std::unique_ptr<UnknownFrontendTranslator>
     createArch(uir::Context &C, const std::string &BinaryFile, const std::string &SymbolFile);
--- src/UnknownFrontend/TranslatorImpl.h
+++ src/UnknownFrontend/TranslatorImpl.h
@@ -39,28 +39,38 @@ protected:
 public:
     // Translate
     // Translate the given binary into UnknownIR
-    virtual std::unique_ptr<uir::Module> translateBinary(const std::string &ModuleName) = 0;
+    virtual std::unique_ptr<uir::Module> translateBinary(const std::string &ModuleName) override { return {}; }
 
     // Translate one instruction into UnknownIR
-    virtual bool translateOneInstruction(const uint8_t *Bytes, size_t Size, uint64_t Address, uir::BasicBlock *BB) = 0;
+    virtual bool
+    translateOneInstruction(const uint8_t *Bytes, size_t Size, uint64_t Address, uir::BasicBlock *BB) override
+    {
+        return false;
+    }
     virtual bool translateOneInstruction(const cs_insn *Insn, uint64_t Address, uir::BasicBlock *BB) = 0;
 
     // Translate one BasicBlock into UnknownIR
-    virtual uir::BasicBlock *translateOneBasicBlock(const std::string &BlockName, uint64_t Address) = 0;
+    virtual uir::BasicBlock *translateOneBasicBlock(const std::string &BlockName, uint64_t Address) override
+    {
+        return nullptr;
+    }
 
     // Translate one Function into UnknownIR
-    virtual uir::Function *translateOneFunction(const std::string &FunctionName, uint64_t Address) = 0;
+    virtual uir::Function *translateOneFunction(const std::string &FunctionName, uint64_t Address) override
+    {
+        return nullptr;
+    }
 
 public:
     // Get/Set
     // Get the context of this translator
-    uir::Context &getContext() const;
+    virtual uir::Context &getContext() const override;
 
     // Get the Binary File
-    const std::string &getBinaryFile() const;
+    virtual const std::string &getBinaryFile() const override;
 
     // Get the Symbol File
-    const std::string &getSymbolFile() const;
+    virtual const std::string &getSymbolFile() const override;
 
     // Get the capstone handle
     csh getCapstoneHandle() const;
@@ -69,16 +79,16 @@ public:
     void setCapstoneHandle(csh CapstoneHandle);
 
     // Get the begin of current pointer
-    const uint64_t getCurPtrBegin() const;
+    virtual const uint64_t getCurPtrBegin() const override;
 
     // Get the end of current pointer
-    const uint64_t getCurPtrEnd() const;
+    virtual const uint64_t getCurPtrEnd() const override;
 
     // Set the begin of current pointer
-    void setCurPtrBegin(uint64_t Ptr);
+    virtual void setCurPtrBegin(uint64_t Ptr) override;
 
     // Set the end of current pointer
-    void setCurPtrEnd(uint64_t Ptr);
+    virtual void setCurPtrEnd(uint64_t Ptr) override;
 };
 
 } // namespace ufrontend
