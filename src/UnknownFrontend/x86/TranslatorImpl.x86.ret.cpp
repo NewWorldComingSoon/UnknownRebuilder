5f8256fe60f7e9f42b49ad5e1b64e62e15cc67b4
gmh5225 <2315157@qq.com>
2023-01-16 02:41:37 +0800

    Update

--- src/UnknownFrontend/x86/TranslatorImpl.x86.ret.cpp
+++ src/UnknownFrontend/x86/TranslatorImpl.x86.ret.cpp
@@ -13,6 +13,31 @@ UnknownFrontendTranslatorImplX86::translateRetInstruction(const cs_insn *Insn, u
         return false;
     }
 
+    auto &X86Info = Insn->detail->x86;
+    if (X86Info.op_count == 0)
+    {
+        // ret
+        uir::IRBuilder IBR(BB);
+
+        IBR.createRetVoid(Insn->address);
+    }
+    else if (X86Info.op_count == 1)
+    {
+        // ret imm
+        uir::IRBuilder IBR(BB);
+
+        const uint32_t TypeSize = 16;
+        auto Imm = static_cast<uint64_t>(X86Info.operands[0].imm.imm);
+
+        IBR.createRetImm(
+            uir::ConstantInt::get(uir::Type::getIntNTy(getContext(), TypeSize), unknown::APInt(TypeSize, Imm)),
+            Insn->address);
+    }
+    else
+    {
+        return false;
+    }
+
     return true;
 }
 
