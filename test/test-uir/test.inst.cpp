a64c87f563782b6bf9e58f7c2a41662e5fa09ece
gmh5225 <2315157@qq.com>
2023-01-26 03:26:52 +0800

    Update

--- test/test-uir/test.inst.cpp
+++ test/test-uir/test.inst.cpp
@@ -15,6 +15,9 @@ TEST(test_uir, test_uir_inst_RetIMM_1)
 
     auto ReturnImmInst = ReturnImmInstruction::get(CTX, ConstantInt::get(CTX, unknown::APInt(32, 1)));
     ReturnImmInst->setInstructionAddress(0x401000);
+
+    ReturnImmInst->enablePrintOp();
+
     ReturnImmInst->print(unknown::outs());
     unknown::outs() << *ReturnImmInst;
 
@@ -52,6 +55,9 @@ TEST(test_uir, test_uir_inst_Store_1)
 
     auto StoreInst = StoreInstruction::get(CTX, Val, Ptr);
     StoreInst->setInstructionAddress(0x401000);
+
+    StoreInst->enablePrintOp();
+
     StoreInst->print(unknown::outs());
     unknown::outs() << *StoreInst;
 
@@ -72,7 +78,11 @@ TEST(test_uir, test_uir_inst_Ret_1)
 
     auto RetInst = ReturnInstruction::get(CTX);
     RetInst->setInstructionAddress(0x401000);
+
+    RetInst->enablePrintOp();
+
     RetInst->print(unknown::outs());
+
     unknown::outs() << *RetInst;
 }
 
@@ -86,7 +96,11 @@ TEST(test_uir, test_uir_inst_JmpAddr_1)
 
     auto JmpAddrInst = JmpAddrInstruction::get(CTX, ConstantInt::get(CTX, unknown::APInt(64, 0x406000)));
     JmpAddrInst->setInstructionAddress(0x401000);
+
+    JmpAddrInst->enablePrintOp();
+
     JmpAddrInst->print(unknown::outs());
+
     unknown::outs() << *JmpAddrInst;
 }
 
@@ -101,6 +115,7 @@ TEST(test_uir, test_uir_inst_JmpBB_1)
     auto BB = BasicBlock::get(CTX, "bb1", 0x406000, 0x406005);
     auto JmpBBInst = JmpBBInstruction::get(CTX, BB);
     JmpBBInst->setInstructionAddress(0x401000);
+    JmpBBInst->enablePrintOp();
     JmpBBInst->print(unknown::outs());
     unknown::outs() << *JmpBBInst;
 }
@@ -122,6 +137,7 @@ TEST(test_uir, test_uir_inst_JccAddr_1)
 
     auto JccAddrInst = JccAddrInstruction::get(CTX, Addr1, Addr2, FlagsVar);
     JccAddrInst->setInstructionAddress(0x401000);
+    JccAddrInst->enablePrintOp();
     JccAddrInst->print(unknown::outs());
     unknown::outs() << *JccAddrInst;
 }
@@ -143,6 +159,33 @@ TEST(test_uir, test_uir_inst_JccBB_1)
 
     auto JccBBInst = JccBBInstruction::get(CTX, BB1, BB2, FlagsVar);
     JccBBInst->setInstructionAddress(0x401000);
+    JccBBInst->enablePrintOp();
     JccBBInst->print(unknown::outs());
     unknown::outs() << *JccBBInst;
+}
+
+TEST(test_uir, test_uir_inst_GBP_1)
+{
+    Context CTX;
+    CTX.setArch(Context::Arch::ArchX86);
+    CTX.setMode(Context::Mode::Mode64);
+
+    std::cout << std::format("GBPComponent = {}", GBPComponent.mOpCodeName.data()) << std::endl;
+
+    auto Ptr = LocalVariable::get(Type::getInt32PtrTy(CTX), "ptr1", 0x601000);
+    auto BitIndex = ConstantInt::get(Type::getInt32Ty(CTX), unknown::APInt(32, 8));
+
+    auto GBPInst = GetBitPtrInstruction::get(Type::getInt8PtrTy(CTX), Ptr, BitIndex);
+    GBPInst->setInstructionAddress(0x401000);
+
+    GBPInst->enablePrintOp();
+
+    GBPInst->print(unknown::outs());
+    unknown::outs() << *GBPInst;
+
+    for (auto OpIt = GBPInst->op_begin(); OpIt != GBPInst->op_end(); ++OpIt)
+    {
+        auto Op = *OpIt;
+        std::cout << std::format("Op = {}", Op->getName()) << std::endl;
+    }
 }
